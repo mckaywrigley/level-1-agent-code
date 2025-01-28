d2ea29de84f6aef6b1310bee5aa4b6edbac3db4b
Mckay Wrigley <29221284+mckaywrigley@users.noreply.github.com>
2025-01-27 18:28:16 -0700

    add github experiment

--- index.ts
+++ index.ts
@@ -4,19 +4,56 @@ import OpenAI from "openai";
 config({ path: ".env.local" });
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY
+  baseURL: "https://openrouter.ai/api/v1",
+  apiKey: process.env.OPENROUTER_API_KEY
+  // baseURL: "https://api.deepseek.com",
+  // apiKey: process.env.DEEPSEEK_API_KEY
+  // baseURL: "https://api.groq.com/openai/v1",
+  // apiKey: process.env.GROQ_API_KEY
 });
 
-async function main() {
-  const response = await openai.chat.completions.create({
-    model: "gpt-4o-mini",
-    messages: [
-      { role: "system", content: "You are a helpful assistant." },
-      { role: "user", content: "Hello!" }
-    ]
-  });
+// const enhancedModel = wrapLanguageModel({
+//   model: groq("deepseek-r1-distill-llama-70b"),
+//   middleware: extractReasoningMiddleware({ tagName: "think" })
+// });
 
-  console.log(response.choices[0].message.content);
+async function main() {
+  console.log("Starting...");
+  try {
+    const response = await openai.chat.completions.create({
+      // model: "deepseek-reasoner",
+      model: "deepseek/deepseek-r1",
+      messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        { role: "user", content: "How do I connect to the gmail api using typescript?" }
+      ]
+      // stream: true
+    });
+
+    console.log(response);
+
+    // for await (const chunk of response) {
+    //   console.log(chunk.choices[0].delta);
+    // }
+
+    // const { text, reasoning, usage, ...rest } = await generateText({
+    //   model: enhancedModel,
+    //   messages: [
+    //     { role: "system", content: "You are a helpful assistant." },
+    //     { role: "user", content: "How do I connect to the gmail api using typescript?" }
+    //   ]
+    // });
+    // console.log("-- REST --\n", rest);
+    // console.log("\n\n\n--------------------------------\n\n\n");
+    // console.log("-- TEXT --\n", text);
+    // console.log("\n\n\n--------------------------------\n\n\n");
+    // console.log("-- REASONING --\n", reasoning);
+    // console.log("\n\n\n--------------------------------\n\n\n");
+    // console.log("-- USAGE --\n", usage);
+    // console.log("\n\n\n--------------------------------\n\n\n");
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 main();
